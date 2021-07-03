311027750d47f6ac74a63a11e6f56fd97e892632
phongng255 <72609283+phongng255@users.noreply.github.com>
2021-07-03 19:59:54 +0700

    Initial commit

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,44 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Create()
+        {
+            BigSchoolContext context = new BigSchoolContext();
+            Course obj = new Course();
+            obj.listcategory = context.Categories.ToList();
+            return View(obj);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Course objCourse)
+        {
+            BigSchoolContext context = new BigSchoolContext();
+            ModelState.Remove("LecturerID");
+            if (!ModelState.IsValid)
+            {
+                objCourse.listcategory = context.Categories.ToList();
+                return View("Create", objCourse);
+            }
+
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            objCourse.LecturerID = user.Id;
+
+            context.Courses.Add(objCourse);
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+     
+
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,29 @@
+﻿namespace BigSchool.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Course")]
+    public partial class Course
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string LecturerID { get; set; }
+        [Required(ErrorMessage = "Place không được để trống")]
+        [StringLength(255)]
+        public string Place { get; set; }
+        [Required(ErrorMessage = "DateTime không được để trống")]
+        public DateTime DateTime { get; set; }
+
+        public int CategoryID { get; set; }
+        public List<Category> listcategory = new List<Category>();
+        public virtual Category Category { get; set; }
+        public string Name;
+    }
+}
